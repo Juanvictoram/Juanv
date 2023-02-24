14131d59da6ee3b8ec976d30cc8da9451e9fc59d
mscodex <99851430+mscodex@users.noreply.github.com>
2023-02-24 04:55:03 -0300

    versio1.3

--- blaze_double.py
+++ blaze_double.py
@@ -1,119 +1,285 @@
-import telebot
+import datetime
+import time
 
 
-token_bot = "TOKEN DO BOT"
-chat_id = "CHAT ID DO CANAL\GRUPO"
-bot = telebot.TeleBot(token_bot)
-
-bot.send_message(chat_id, text="Online")
-
-analise_open = False
-entrada_atual = 0
-max_gale = int(input("Quantos martingale: "))
-
-def reset():
-    global analise_open
-    global entrada_atual
-
-    analise_open = False
-    entrada_atual = 0
-
-    return
-
-def win():
-    bot.send_message(chat_id, text=f'''
-WIN
-    ''')
-    reset()
-    return
-
-def loss():
-    bot.send_message(chat_id, text=f'''
-LOSS
-    ''')
-    reset()
-    return
-
-def branco():
-    bot.send_message(chat_id, text=f'''
-BRANCO
-    ''')
-    reset()
-    return
-
-def martingale():
-    global entrada_atual
-    entrada_atual += 1
-
-    if entrada_atual <= max_gale:
-        bot.send_message(chat_id, text=f'''
-Vamos para o gale {entrada_atual}
-    ''')
+class utils:
     
-    else:
-        loss()
-        reset()
+    def get_data(self):
+        self.now = str(datetime.datetime.now().strftime("%d/%m/%Y"))
+        self.checknow = self.now
+        self.nowtime = int(datetime.datetime.now().strftime("%H"))
 
-    return
+    def send_sinal(self):
+        print("Sinal enviado...")
+        self.bot.send_message(chat_id=self.user_id, text=(f'''
+🎲 *ENTRADA CONFIRMADA!*
 
-def send_result(resultado, color):
-    if resultado[0] >= 1 and resultado[0] <= 7 and color == "🟥":
-        win()
+🎰 Apostar no {self.direction_color}
+🟡 Proteger o empate (Meio) 
+🔁 Fazer até 2 gales
+
+📱 *{self.name}*
+'''))
         return
 
-    elif resultado[0] >= 1 and resultado[0] <= 7 and color == "⬛️":
-        martingale()
+    def alert(self):
+        message_id = self.bot.send_message(
+            self.user_id, text='''
+    ⚠️ Vamos para o %iª GALE ⚠️
+            ''' % (self.entrada_atual)).message_id
+        self.message_ids = message_id
+        self.message_delete = True
         return
-    
-    elif resultado[0] >= 8 and resultado[0] <= 14 and color == "⬛️":
-        win()
+
+    def alert_sinal(self):
+        message_id = self.bot.send_message(
+            self.user_id, text='''
+⚠️ ANALISANDO, FIQUE ATENTO!!!
+''').message_id
+        self.message_ids = message_id
+        self.message_delete = True
         return
 
-    elif resultado[0] >= 8 and resultado[0] <= 14 and color == "🟥":
-        martingale()
-        return
-    
-    else:
-        branco()    
-    return
+    def delet(self):
+        if self.message_delete == True:
+            self.bot.delete_message(chat_id=self.user_id,
+                                message_id=self.message_ids)
+            self.message_delete = False
 
-def send_sinal(color):
-    bot.send_message(chat_id, text=f'''
-Sinal ENVIADO   
-ENTRAR: {color}
-    ''')
-
-    return
-
-def estrategy(resultado):
-    global analise_open
-    global color_sinal
-
-    lista_ultimas_cores = []
-    
-    for i in resultado:
-        if i >= 1 and i <= 7:
-            cor = "V"
-            lista_ultimas_cores.append(cor)
-        elif i >= 8 and i <= 14:
-            cor = "P"
-            lista_ultimas_cores.append(cor)
+    def results(self):
+        if self.win_results + self.loss_results != 0:
+            a = 100 / (self.win_results + self.loss_results) * self.win_results
         else:
-            cor = "B"
-            lista_ultimas_cores.append(cor)
-    
-    print(lista_ultimas_cores)
+            a = 0
 
-    if analise_open == True:
-        send_result(resultado, color_sinal)
-    else:
-        if lista_ultimas_cores[0:3] == ["P", "P", "V"]:
-            color_sinal = "🟥"
-            send_sinal(color_sinal)
-            analise_open = True
+        self.win_hate = (f'{a:,.2f}%')
+        self.bot.send_message(chat_id=self.user_id, text=(f'''
+📊 RESULTADO GERAL 
+           ✅{self.win_results} VS 🚫{self.loss_results} 
 
-        if lista_ultimas_cores[0:3] == ["V", "V", "P"]:
-            color_sinal = "⬛️"
-            send_sinal(color_sinal)
-            analise_open = True
+► Win sem gale =  {self.win_semgale}
+► Win 1ª gale  =  {self.win_gale1}
+► Win 2ª gale  =  {self.win_gale2}
+► Consecutivas = {self.max_hate}
+► Assertividade = {self.win_hate}
+
+👨🏾‍💻
+    '''))
+        return
+
+    def restart(self):
+        if self.now != self.checknow:
+            print('Reload')
+            self.bot.send_sticker(
+                self.user_id, sticker='CAACAgEAAxkBAAEBbJJjXNcB92-_4vp2v0B3Plp9FONrDwACvgEAAsFWwUVjxQN4wmmSBCoE')
+            utils.get_data(self)
+            self.checknowtime = self.nowtime
+            utils.results(self)
+
+            self.win_results = 0
+            self.loss_results = 0
+            self.win_semgale = 0
+            self.win_gale1 = 0
+            self.win_gale2 = 0
+            self.max_hate = 0
+            self.empate_results = 0
+
+            time.sleep(10)
+            self.bot.send_sticker(
+                self.user_id, sticker='CAACAgEAAxkBAAEBPQZi-ziImRgbjqbDkPduogMKzv0zFgACbAQAAl4ByUUIjW-sdJsr6CkE')
+            utils.get_data(self)
+            self.checknowtime = self.nowtime
+            utils.results(self)
+            return True
+        else:
+            return False
+
+    def reset(self):
+        self.analise_open = 0
+        self.analisar = 0
+        self.entrada_atual = 0
+
+        if not utils.restart(self):
+            utils.results(self)
+        return
+
+    def martingale(self):
+        if self.result == 'B':
+            if self.entrada_atual == 0:
+                self.max_hate += 1
+                self.win_results += 1
+                self.empate_results += 1
+                self.empate_semgale += 1
+                self.win_semgale += 1
+                self.bot.send_message(chat_id=self.user_id, text=(f'''
+    ⚪️⚪️⚪️ BRANCO ⚪️⚪️⚪️
+        {self.empate_results}ª BRANCO DO DIA!!!
+    '''))
+                utils.reset(self)
+                return
+
+            if self.entrada_atual == 1:
+                self.max_hate += 1
+                self.win_results += 1
+                self.empate_results += 1
+                self.empate_gale1 += 1
+                self.win_gale1 += 1
+                self.bot.send_message(chat_id=self.user_id, text=(f'''
+    ⚪️⚪️⚪️ BRANCO ⚪️⚪️⚪️
+        {self.empate_results}ª BRANCO DO DIA!!!
+    '''))
+                utils.reset(self)
+                return
+
+            if self.entrada_atual == 2:
+                self.max_hate += 1
+                self.win_results += 1
+                self.empate_results += 1
+                self.empate_gale2 += 1
+                self.win_gale1 += 1
+                self.bot.send_message(chat_id=self.user_id, text=(f'''
+    ⚪️⚪️⚪️ BRANCO ⚪️⚪️⚪️
+        {self.empate_results}ª BRANCO DO DIA!!!
+    '''))
+                utils.reset(self)
+                return
+
+        if self.result == 'V' and self.direction_color == '🔴':
+            if self.entrada_atual == 0:
+                self.win_results += 1
+                self.max_hate += 1
+                self.win_semgale += 1
+                self.bot.send_message(chat_id=self.user_id, text=(f'''
+✅✅✅ GREEN!!! ✅✅✅
+(🔴)'''))
+                utils.reset(self)
+                return
+
+            if self.entrada_atual == 1:
+                self.win_results += 1
+                self.max_hate += 1
+                self.win_gale1 += 1
+                self.bot.send_message(chat_id=self.user_id, text=(f'''
+✅✅✅ GREEN!!! ✅✅✅
+(⚫️🔴)'''))
+                utils.reset(self)
+                return
+
+            if self.entrada_atual == 2:
+                self.win_results += 1
+                self.max_hate += 1
+                self.win_gale2 += 1
+                self.bot.send_message(chat_id=self.user_id, text=(f'''
+✅✅✅ GREEN!!! ✅✅✅
+(⚫️⚫️🔴)'''))
+                utils.reset(self)
+                return
+
+        if self.result == 'P' and self.direction_color == '⚫️':
+            if self.entrada_atual == 0:
+                self.win_results += 1
+                self.max_hate += 1
+                self.win_semgale += 1
+                self.bot.send_message(chat_id=self.user_id, text=(f'''
+✅✅✅ GREEN!!! ✅✅✅
+(⚫️)'''))
+                utils.reset(self)
+                return
+
+            if self.entrada_atual == 1:
+                self.win_results += 1
+                self.max_hate += 1
+                self.win_gale1 += 1
+                self.bot.send_message(chat_id=self.user_id, text=(f'''
+✅✅✅ GREEN!!! ✅✅✅
+(🔴⚫️)'''))
+                utils.reset(self)
+                return
+
+            if self.entrada_atual == 2:
+                self.win_results += 1
+                self.max_hate += 1
+                self.win_gale2 += 1
+                self.bot.send_message(chat_id=self.user_id, text=(f'''
+✅✅✅ GREEN!!! ✅✅✅
+(🔴🔴⚫️)'''))
+                utils.reset(self)
+                return
+
+        if self.result == 'P' and self.direction_color == '🔴':
+            if self.entrada_atual == 0:
+                self.entrada_atual += 1
+                utils.alert(self)
+                return
+
+            if self.entrada_atual == 1:
+                self.entrada_atual += 1
+                utils.alert(self)
+                return
+
+            if self.entrada_atual == 2:
+                self.loss_results += 1
+                self.bot.send_message(chat_id=self.user_id, text=(f'''
+Loss🚫'''))
+                utils.reset(self)
+                self.max_hate = 0
+                return
+
+        if self.result == 'V' and self.direction_color == '⚫️':
+            if self.entrada_atual == 0:
+                self.entrada_atual += 1
+                utils.alert(self)
+                return
+
+            if self.entrada_atual == 1:
+                self.entrada_atual += 1
+                utils.alert(self)
+                return
+
+            if self.entrada_atual == 2:
+                self.loss_results += 1
+                self.bot.send_message(chat_id=self.user_id, text=(f'''
+Loss🚫'''))
+                utils.reset(self)
+                self.max_hate = 0
+                return
+
+    def estrategy(self, finalcor):
+        lista = []
+
+        for i in finalcor:
+            if i >= 1 and i <= 7:
+                lista.append("V")
+            elif i >= 8 and i <= 17:
+                lista.append("P")
+            else:
+                lista.append("B")
+
+
+        if self.analisar == 1:
+            self.result = lista[0]
+            utils.martingale(self)
+            return
+        
+        elif self.analisar == 0:
+            if lista[0:3] == ['V', 'V', 'P']:
+                self.direction_color = '⚫️'
+                self.analisar = 1
+                utils.send_sinal(self)
+                return
+
+            if lista[0:3] == ['P', 'P', 'V']:
+                self.direction_color = '🔴'
+                self.analisar = 1
+                utils.send_sinal(self)
+                return
+
+            if lista[0:2] == ['V', 'P']:
+                utils.alert_sinal(self)
+                return
+
+            if lista[0:2] == ['P', 'V']:
+                utils.alert_sinal(self)
+                return
+
 
